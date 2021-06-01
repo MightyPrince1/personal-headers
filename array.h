c89953e49d54e39beb93ee91ca57d48350a96782
MightyPrince1 <maximilian.brandtner@gmail.com>
2021-06-01 22:34:56 +0700

    functions that are supposed to handle memory allocation(WIP)(not working yet)

---
+++ array.h
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#ifndef ARRAY_H
+#define ARRAY_H
+
+#ifdef __CPLUSPLUS
+extern "C"{
+#endif
+void free(void *array){
+
+}
+
+void resize(void *array, int a){
+
+}
+
+void Resize2D(void *array, int a, int b){
+
+}
+
+void Resize3D(void *array, int a, int b, int c){
+
+}
+
+int LenghtOfArray(void *array){
+	return (int)( sizeof(array) / sizeof(array[0]));
+}
+
+#ifdef __CPLUSPLUS
+}
+#endif
+
+#endif
