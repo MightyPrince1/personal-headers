fb9d0eafa5f0719b0e3b48f35a48d76c20817fed
MightyPrince1 <maximilian.brandtner@gmail.com>
2021-06-08 17:42:39 +0700

    made WorldMap RGB compatible

--- world.h
+++ world.h
@@ -20,7 +20,7 @@ void DefaultWorldMap(){
   for(int i = 0; i < 1000; i ++){
     for(int j = 0; j < 1000; j ++){
       for(int k = 0; k < 1000; k ++){
-        WorldMap[i][j][k] = default_WorldState;
+        WorldMap[i][j][k][3] = default_WorldState;
       }
     }
   }
