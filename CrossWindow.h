47977d22c04b5e01a3fd65fa16524ae33a01ee6b
MightyPrince1 <maximilian.brandtner@gmail.com>
2021-06-11 16:16:07 +0700

    will be doing advanced window things in the future for now just makes writing code faster

---
+++ CrossWindow.h
@@ -0,0 +1,22 @@
+#ifndef CROSSWINDOW_H
+#define CROSSWINDOW_H
+
+#ifdef __cplusplus
+  extern "C"{
+#endif
+
+
+
+#define USING_WINDOW
+
+#include "CrossPlatform.h"
+
+void WindowVectorize()
+
+void WindowUpdate()
+
+#ifdef __cplusplus
+  }
+#endif
+
+#endif
