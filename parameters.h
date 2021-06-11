e3115cd07bfab5e7cfda9c46101a0a4ecb40c771
MightyPrince1 <maximilian.brandtner@gmail.com>
2021-06-11 16:12:06 +0700

    a list of parameters for the headers

---
+++ parameters.h
@@ -0,0 +1,25 @@
+/*
+list of parameters of headers
+
+Do not use it
+*/
+
+#ifndef PARAMETERS_H
+#define PARAMETERS_H
+
+#ifdef __cplusplus
+  extern "C"{
+#endif
+
+//CrossPlatform
+#define USING_WINDOW
+#define ERROR_RESULTS_IN_EXIT
+#define USING_TERMINAL_INPUT
+
+//ConsoleScreen
+#define DISABLE_CONSOLE_SCREEN_IF_USING_WINDOW
+
+#ifdef __cplusplus
+  }
+#endif
+#endif
