dc13eff68f7d13a83d6f2b152e2aafa688d122df
MightyPrince1 <maximilian.brandtner@gmail.com>
2021-06-18 17:44:41 +0700

    groundwork for both functions (not working yet)

--- array.h
+++ array.h
@@ -7,25 +7,35 @@ extern "C"{
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdarg.h>
-
-void delete(int *array){
-
-}
-
-void resize(void *array, int ArraySizes){
-	va_list varlist;
-
-	va_start(varlist, ArraySizes);
-
-	va_arg(varlist, int);
-
-	va_end(varlist);
-}
+#include "AdMath.h"
 
 int LenghtOfArray(void *array){
 	return (int)( sizeof(array) / sizeof(array[0]));
 }
+
+void delete(int &array){
+	if(&array){
+		free(&array);
+		&array = NULL;
+	}
+}
+
+void resize(int &array, int array_size_now[], array_size_wanted[]){
+	if(LenghtOfArray(*array) != LenghtOfArray(*array_size_now)){
+		#ifdef ERROR_RESULTS_IN_EXIT
+			exit(1);
+		#endif
+
+		return;
+	}
+
+	int n_dimensions =  min(LenghtOfArray(*array_size_now), LenghtOfArray(*array_size_wanted));
+	int n_dimensions_wanted = LenghtOfArray(*array_size_wanted);
+	int n_dimensions_now = LenghtOfArray(*array_size_now);
+
+
+}
+
 
 #ifdef __CPLUSPLUS
 }
