5e46bf698c95203af278aa81e9e1456ff211601a
MightyPrince1 <maximilian.brandtner@gmail.com>
2021-07-02 14:07:53 +0700

    added CircleCircumference() and improved performance

--- implementations.h
+++ implementations.h
@@ -1,5 +1,6 @@
 /*
   (int / use as bool) CircleBool(r)
+  (float)             CricleCircumference(r)
   //returns *array[r * 2 +1][r * 2 + 1]
 */
 
@@ -10,7 +11,7 @@
   extern "C"{
 #endif
 
-#include <math.h>
+#include "AdMath.h"
 
 int CircleBool(int r){
   int x_circle_axis = r * 2 + 1;
@@ -31,17 +32,9 @@ int CircleBool(int r){
 
   for(int i = 0; i < x_f; i ++){
     for(int j = 0; j < y_f; j ++){
-      float tmp = pow((r - i), 2) + pow((j), 2);
-      if(sqrt(tmp) <= r){
-        fract[i][j] = 1;
-      }
-      else{
-        fract[i][j] = 0;
-      }
+      float tmp_root = p_theorum(r - i, j);
 
-      if(i > x_circle_axis  - 2 * y_f){
-        circle[i][i] = 1;
-      }
+      fract[i][j] = 1 * (tmp_root <= r);
     }
   }
   //apply
@@ -70,7 +63,54 @@ int CircleBool(int r){
   return *circle;
 }
 
-int Circle(int r){
+float CircleCircumference(int r){
+  int x_circle_axis = r * 2 + 1;
+  int y_circle_axis = x_circle_axis;
+
+  int y_f = 0.7071057812 * r;
+  int x_f = round(r - y_f);
+
+  float circle[x_circle_axis][y_circle_axis];
+
+
+  float fract[x_f][y_f];
+  for(int i = 0; i < x_circle_axis; i ++){
+    for(int j = 0; j < y_circle_axis; j ++){
+      circle[i][j] = -1;
+    }
+  }
+
+  for(int i = 0; i < x_f; i ++){
+    for(int j = 0; j < y_f; j ++){
+      float tmp_root = p_theorum(r - i, j);
+
+      fract[i][j] = (2 * 3.141592653589793 * tmp * (tmp_root <= r)) + (-1 * tmp_root > r);
+    }
+  }
+  //apply
+  for(int i = 0; i < x_f; i ++){
+    for(int j = 0; j < y_f; j ++){
+      circle[i][j + r] = fract[i][j];
+      circle[i][r - j] = fract[i][j];
+      circle[2 * r - i][r + j] = fract[i][j];
+      circle[2 * r - i][r - j] = fract[i][j];
+      circle[r + j][i] = fract[i][j];
+      circle[r - j][i] = fract[i][j];
+      circle[r + j][2 * r - i] = fract[i][j];
+      circle[r - j][2 * r - i] = fract[i][j];
+
+    }
+  }
+
+  for(int i = x_f; i < (2 * r - x_f) + 1; i ++){
+    for(int j = x_f; j < (2 * r - x_f) + 1; j ++){
+      circle[i][j] = 2 * 3.141592653589793 * pow((r - i), 2) + pow((j), 2);
+    }
+  }
+
+
+
+  return *circle;
 
 }
 
