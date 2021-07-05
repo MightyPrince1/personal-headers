f3dfdd36bfe2b07ced593daa8d3b3a6a1af5b7ca
MightyPrince1 <maximilian.brandtner@gmail.com>
2021-07-05 12:54:28 +0700

    added equals() and documentation

--- array.h
+++ array.h
@@ -1,3 +1,9 @@
+/*
+(int)												LenghtOfArray(*array)
+														delete(&array);
+(double / use as whatever)	equals(value, dimensions_of_array[])
+*/
+
 #ifndef ARRAY_H
 #define ARRAY_H
 
@@ -20,6 +26,25 @@ void delete(int &array){
 	}
 }
 
+double equals(double value, int dimensions_of_array[]){
+	int Lenght_of_dimesions_of_array = LenghtOfArray(*dimensions_of_array);
+
+	int number_of_elememts_of_result_array = (Lenght_of_dimesions_of_array != 0);
+
+	for(int i = 0; i < Lenght_of_dimesions_of_array; i ++){
+		number_of_elememts_of_result_array = number_of_elememts_of_result_array * dimensions_of_array[i];
+	}
+
+	double result_array[number_of_elememts_of_result_array];
+
+	for(int i = 0; i < number_of_elememts_of_result_array; i ++){
+		result_array[i] = value;
+	}
+
+	return *result_array;
+}
+
+//WIP
 void resize(int &array, int array_size_now[], array_size_wanted[]){
 	if(LenghtOfArray(*array) != LenghtOfArray(*array_size_now)){
 		#ifdef ERROR_RESULTS_IN_EXIT
