57a9cc2ebcf3e915de2820d998f5f14c20e5b62a
MightyPrince1 <maximilian.brandtner@gmail.com>
2021-07-12 20:54:06 +0700

    added RoundBetween() and RoundInterval()

--- AdMath.h
+++ AdMath.h
@@ -4,10 +4,11 @@
 
 (int) 	IsEven(number)
 (int)		NegativeToPositive(number)
-(int) 	power(number,PowerOf)
 (int)	 	min(a,b)
 (int) 	max(a,b)
 (float) p_theorum(a,b)
+(double)RoundBetween(int number, lower, upper)
+(int)		RoundIntervals(int number, int interval);
 (int)		IntRoot(number,RootOf)	//broken
 
 */
@@ -21,17 +22,24 @@ extern "C"{
 
 #include <math.h>
 
+
+
 int IsEven(int number){
 	int remainder = number % 2;
 
 	return 1 * (remainder == 0);
 }
 
+
+
 int NegativeToPositive(int number){
 	return -number * (number < 0) + number * !(number < 0);
 }
 
-int power(int number, int PowerOf){
+
+
+//redundant !!!
+/*int power(int number, int PowerOf){
 	int result = 1;
 
 	for(int i = 0; i < NegativeToPositive(PowerOf); i ++){
@@ -39,16 +47,21 @@ int power(int number, int PowerOf){
 	}
 
 	return (1/result) * (PowerOf < 0) + result * !(PowerOf < 0);
-}
+}*/
+
 
 
 int min(int a,int b){
   return a * (a <= b) + b * (b < a);
 }
 
+
+
 int max(int a,int b){
   return a * (a >= b) + b * (b > a);;
 }
+
+
 
 float p_theorum(float a,float b){
 
@@ -57,21 +70,61 @@ float p_theorum(float a,float b){
   return sqrt(tmp);
 }
 
+
+
 int RadiansToDegrees(int radians){
 	return radians * 180 / M_PI;
 }
+
+
 
 int DegreesToRadians(int degrees){
 	return degrees * M_PI / 180;
 }
 
+
+
 int NotNegative(int number){
 	return number * (number > 0);
 }
 
+
+
 int InBunds(int number, int lower_limit, int upper_limit){
 	return number * (number >= lower_limit && number <= upper_limit) + lower_limit * (number < lower_limit) + upper_limit * (number > upper_limit);
 }
+
+
+
+double RoundBetween(double number, double lower, double upper){
+	while(number > upper){
+		number = number - (upper - lower);
+	}
+
+	return lower * (number < (lower + upper) / 2) + upper * (number >= (lower + upper) / 2);
+}
+
+
+
+int RoundIntervals(int number, int interval){
+	if(interval == 0 || number == 0){
+		return 0;
+	}
+
+	int mulitplier = 0;
+
+	while(number < 0 * (number > interval * multiplier) + number > 0 * (number < interval * multiplier)){
+		multiplier = multiplier + (number > 0) - (number < 0);
+	}
+
+	int InBetween = (number < 0) * (number > (interval * mulitplier)) + (number > 0) * (number > (interval * mulitplier));
+
+	multiplier = mulitplier * (InBetween == 0 && !(number < ((interval * multiplier) + (interval * multiplier)) / 2)) + multiplier * !(InBetween == 0 && !(number < ((interval * multiplier) + (interval * multiplier)) / 2))*(multiplier + (number > 0) - (number < 0));
+
+	return interval * multiplier;
+}
+
+
 
 //somehow broken
 int IntRoot(int number, int PowerOf){
