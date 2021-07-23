2ca04d63f750f860a9fb16af22e1256e88e76695
MightyPrince1 <maximilian.brandtner@gmail.com>
2021-07-23 22:36:37 +0700

    added WindowUpdate() and WindowSpaceWrite() CrossWindow.h can now be used properly

--- CrossWindow.h
+++ CrossWindow.h
@@ -1,3 +1,9 @@
+/*
+hex WindowSpace[x][y]
+    ResizeWindowSpace(x,y)
+    WindowSpaceWrite(x,y,r,g,b)
+    WindowUpdate()
+*/
 #ifndef CROSSWINDOW_H
 #define CROSSWINDOW_H
 
@@ -5,10 +11,13 @@
   extern "C"{
 #endif
 
+#define USING_WINDOW
+
+#include "CrossPlatform.h"
 #include <stdlib.h>
 #include "AdMath.h"
 
-int (**WindowSpace) = NULL;
+hex (**WindowSpace) = NULL;
 
 int WindowSpaceHeight = 0;
 int WindowSpaceWidth = 0;
@@ -16,7 +25,7 @@ int WindowSpaceWidth = 0;
 void ResizeWindowSpace(int x, int y){
 
 
-  int WindowSpaceSave[WindowSpaceHeight][WindowSpaceWidth];
+  hex WindowSpaceSave[WindowSpaceHeight][WindowSpaceWidth];
 
   for(int i = 0; i < WindowSpaceWidth; i ++){
     free(WindowSpace[i]);
@@ -32,10 +41,10 @@ void ResizeWindowSpace(int x, int y){
     }
   }
 
-  WindowSpace = malloc(sizeof(int*) * x);
+  WindowSpace = malloc(sizeof(hex*) * x);
 
   for(int i = 0; i < x; i ++){
-    WindowSpace[i] = malloc(sizeof(int) * y);
+    WindowSpace[i] = malloc(sizeof(hex) * y);
   }
 
   for(int i = 0; i < min(WindowSpaceHeight,x); i++){
@@ -48,20 +57,95 @@ void ResizeWindowSpace(int x, int y){
   WindowSpaceWidth = y;
 }
 
-void WindowSpaceWrite(int x, int y, int hex_value){
+void WindowSpaceWrite(int x, int y, int r,int g, int b){
+  if(x !< WindowSpaceHeight || y !< WindowSpaceWidth){
+      return;
+  }
 
+  WindowSpace[x][y] = CrossRGB(r,g,b);
 }
 
-#define USING_WINDOW
 
-#include "CrossPlatform.h"
-
-void WindowVectorize(){
-
-}
 
 void WindowUpdate(){
+  int n_values = 1;
+  hex (*value_list) = malloc(sizeof(hex));
 
+  value_list[0] = WindowSpace[0][0];
+
+  for(int i = 0; i < WindowSpaceHeight; i++){
+    for(int j = 0; j <WindowSpaceWidth; j++){
+      int broke_cycle = 0;
+
+      for(int k = 0; k < n_values; k ++){
+        if(WindowSpace[i][j] == value_list[k]){
+          broke_cycle = 1;
+          break;
+        }
+      }
+
+      if(broke_cycle == 1){
+        n_values ++;
+        value_list = malloc(sizeof(hex));
+
+        value_list[n_values - 1] = WindowSpace[i][j];
+      }
+    }
+  }
+
+  int value_list_amounts[n_values];
+
+  for(int i = 0; i < WindowSpaceHeight; i ++){
+    for(int j = 0; j < WindowSpaceWidth; j ++){
+      for(int k = 0; k < n_values; k ++){
+        if(WindowsSpace[i][j] == value_list[k]){
+          value_list_amounts[k] ++;
+          break;
+        }
+      }
+    }
+  }
+
+  int max_n_points = 0;
+
+  for(int i = 0; i < n_values; i ++){
+    max_n_points = max(value_list_amounts[k],max_n_points);
+  }
+
+  value_coords[n_values][2][max_n_points];
+
+  for(int i = 0; i < n_values i ++){
+    for(int j = 0; j < 2; j ++){
+      for(int k = 0; k <max_n_points; k ++){
+        value_coords[i][j][k] = -1;
+      }
+    }
+  }
+
+  for(int i = 0; i < WindowSpaceHeight; i ++){
+    for(int j = 0; j < WindowSpaceWidth; j ++){
+      int value_list_position;
+      for(int k = 0; k < n_values; k ++){
+        if(WindowSpace[i][j] == value_list[k]){
+          value_list_position = k;
+          break;
+        }
+      }
+
+      int l_i = 0;
+      while(value_list[l_i] != -1){
+        l_i ++;
+      }
+
+      value_coords[value_list_position][0][l_i] = i;
+      value_coords[value_list_position][1][l_i] = j;
+    }
+  }
+
+  for(int i = 0; i < n_values; i ++){
+    XSetForeground(d,gc,value_list[i]);
+    XDrawPoints(w,d,gc,value_coords[i],max_n_points,CoordModeOrigin);
+  }
 }
 
 #ifdef __cplusplus
